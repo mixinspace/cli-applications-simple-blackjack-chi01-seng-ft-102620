fb2c5a2d9c210c9032b691b08ddf5cf8e4a67b28
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 00:23:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -9,7 +9,14 @@ def deal_card
 end
 
 def display_card_total
-  puts "Your cards add up to #{card_total}"
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+     card_total
+   else
+     invalid_command
+     hit?(card_total)
+   end
   # code #display_card_total here
 end
 
