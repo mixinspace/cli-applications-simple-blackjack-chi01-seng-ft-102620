eeb26a6b08d7a1f9614a09d819d4f0c5a3bb37e6
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 00:21:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,34 +4,44 @@ def welcome
 end
 
 def deal_card
+  rand(1..11)
   # code #deal_card here
 end
 
 def display_card_total
+  puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+   gets.chomp.strip
   # code #get_user_input here
 end
 
 def end_game
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
   # code #initial_round here
 end
 
 def hit?
+  valid_inputs = ["h", "s"]
   # code hit? here
 end
 
 def invalid_command
+   puts "Please enter a valid command"
   # code invalid_command here
 end
 
@@ -40,5 +50,12 @@ end
 #####################################################
 
 def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
   # code runner here
 end
