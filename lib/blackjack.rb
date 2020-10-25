29902faa67cb97fbf8939dda694a17dbb0fc4d4b
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 00:28:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,17 @@ def initial_round
   # code #initial_round here
 end
 
-def hit?
-  valid_inputs = ["h", "s"]
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+    hit?(card_total)
+  end
 end
 
 def invalid_command
