358be1ecefca14ca31708b49778eff2d45b04cd3
Islam Madkaur <islammadkaur@gmail.com>
2020-10-25 00:29:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,7 +22,7 @@ def get_user_input
   # code #get_user_input here
 end
 
-def end_game
+def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
